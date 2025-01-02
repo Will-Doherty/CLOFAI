0f0230eeeb25051a8b2b67bd0993f137e1e7e1ab
Will-Doherty <wdoherty8@gmail.com>
2025-01-02 16:40:18 +0000

    Update load_data_from_hugginface.py

--- CLOFAI/data/load_data_from_hugginface.py
+++ CLOFAI/data/load_data_from_hugginface.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
 
 def load_hf_dset():
-    dataset_dict = load_dataset("willd98/CLOFAI_test")
+    dataset_dict = load_dataset("willd98/CLOFAI")
     return dataset_dict
